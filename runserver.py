2db66a916ed995be6570c53734e81b61b4264119
Ciprian Mandache <mandacheciprian@ymail.com>
2016-07-18 22:50:13 +0300

    Add WSGIContainer through Tornado

--- runserver.py
+++ runserver.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 from fmrapi import app, config
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
-app.run(host='127.0.0.1', port=8088, debug=config.debug)
+http_server = HTTPServer(WSGIContainer(app))
+http_server.bind(8088, address='0.0.0.0')
+http_server.start(0)
+IOLoop.instance().start()
